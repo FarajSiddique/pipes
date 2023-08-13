f9d533161286f2e38252e201dd43737c047d7aaa
Faraj Siddique <fas5225@psu.edu>
2023-08-13 15:55:31 -0400

    Learning angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string;
+  date!: string;
+  amount!: number;
+  miles!: number;
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: number) {
+    this.amount = value;
+  }
 }
